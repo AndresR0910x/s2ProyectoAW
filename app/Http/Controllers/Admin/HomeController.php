ee52841c465d07de1e76155aa6f1fcfff1df3383
AndresR0910x <androd2000x@gmail.com>
2023-06-19 12:32:40 -0500

    5 operaciones by Andres

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,12 +3,89 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\BolsaEmpleo;
 use Illuminate\Http\Request;
+use App\Models\Curso;
+use App\Models\Empleo;
+use App\Models\EquipoLiderazgo;
 
 class HomeController extends Controller
 {
     //
     public function index(){
-        return view('admin.index');
+
+
+        
+
+        
+
+    $dataCursos = $this->dataCur()['dataCursos'];
+    $dataEmpleos = $this->dataEmpleos()['dataEmpleos'];
+    $dataContactos = $this->dataContac()['dataContactos']; 
+    $dataBE = $this->bolsaEmpleos()['dataBE'];
+    $dataEL = $this->dataLiderazgo()['dataEL'];
+
+    return view('admin.index', [
+        "dataCursos" => $dataCursos, 
+        "dataEmpleos" =>$dataEmpleos,
+        "dataContactos" => $dataContactos,
+        "dataBE" => $dataBE, 
+        "dataEL" => $dataEL
+
+    ]);
+    }
+
+    public function dataCur(){
+        $cursos = Curso::all(); 
+        $puntos = []; 
+
+        foreach($cursos as $curso){
+            $puntos[] = [
+                $curso['nombre_curso'], 
+                $curso['duracion_curso']
+            ];
+        }
+        
+        return (["dataCursos" => json_encode($puntos)]);
+    }
+
+    public function dataEmpleos(){
+        $empleos = Empleo::all(); 
+        $puntos = []; 
+        foreach($empleos as $empleo){
+            $puntos[] = [ 
+                'name' => $empleo['nombre_empleo'], 
+                'y' => $empleo['vacantes']
+            ]; 
+        }
+        return (["dataEmpleos" => json_encode($puntos)]);
+    }
+
+
+    public function dataContac(){
+        $equipoLiderazgo = EquipoLiderazgo::all();
+        $cont  = 0;  
+        foreach($equipoLiderazgo as $el){
+                $cont ++;
+        }
+        return (["dataContactos" => $cont]);
+    }
+
+    public function bolsaEmpleos(){
+        $bolsaEmpleos =BolsaEmpleo::all();
+        $cont = 0; 
+        foreach($bolsaEmpleos as $bE){
+            $cont++; 
+        }
+        return (["dataBE" => $cont]); 
+    }
+
+    public function dataLiderazgo(){
+        $eLiderazgo =EquipoLiderazgo::all();
+        $cont = 0; 
+        foreach($eLiderazgo as $bE){
+            $cont++; 
+        }
+        return (["dataEL" => $cont]); 
     }
 }
