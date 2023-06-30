75b21f17b3121088f30b90aa26684fa7f60ee15b
Kenzo0513 <kenneth.acurio.3a@gmail.com>
2023-06-29 19:29:28 -0500

    graficos porcentuales dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,12 +3,77 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\evento;
+use App\Models\horarioCurso;
+use App\Models\horarioEmpleo;
+use App\Models\responsabilidadSocial;
+use App\Models\servicio;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     //
     public function index(){
-        return view('admin.index');
+
+    $dataEventos = $this->dataEventos()['dataEventos']; 
+    $dataHorariosCursos = $this->dataHorariosCursos()['dataHorariosCursos'];
+    $dataHoraiosEmpleos = $this->dataHoraiosEmpleos()['dataHoraiosEmpleos'];
+    $dataResponsabilidadSocial = $this->dataResponsabilidadSocial()['dataResponsabilidadSocial'];
+    $dataServicio = $this->dataServicio()['dataServicio'];
+
+    return view('admin.index', [
+        "dataEventos" => $dataEventos,
+        "dataHorariosCursos" => $dataHorariosCursos, 
+        "dataHoraiosEmpleos" =>$dataHoraiosEmpleos,
+        "dataResponsabilidadSocial" => $dataResponsabilidadSocial, 
+        "dataServicio" => $dataServicio
+
+    ]);
+    }
+
+    public function dataEventos(){
+        $dataEventos = evento::all();
+        $cont  = 0;  
+        foreach($dataEventos as $ev){
+                $cont ++;
+        }
+        return (["dataEventos" => $cont]);
+    }
+
+    public function dataHorariosCursos(){
+        $dataHorariosCursos = horarioCurso::all();
+        $cont  = 0;  
+        foreach($dataHorariosCursos as $HC){
+                $cont ++;
+        }
+        return (["dataHorariosCursos" => $cont]);
+    }
+
+    public function dataHoraiosEmpleos(){
+        $dataHoraiosEmpleos = horarioEmpleo::all();
+        $cont  = 0;  
+        foreach($dataHoraiosEmpleos as $HE){
+                $cont ++;
+        }
+        return (["dataHoraiosEmpleos" => $cont]);
+    }
+
+    public function dataResponsabilidadSocial(){
+        $dataResponsabilidadSocial =responsabilidadSocial::all();
+        $cont = 0; 
+        foreach($dataResponsabilidadSocial as $rS){
+            $cont++; 
+        }
+        return (["dataResponsabilidadSocial" => $cont]); 
+    }
+
+    public function dataServicio(){
+        $dataServicio = servicio::all();
+        $cont = 0; 
+        foreach($dataServicio as $se){
+            $cont++; 
+        }
+        return (["dataServicio" => $cont]); 
     }
 }
+
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,219 @@
 @extends('adminlte::page')
+
+@section('title', 'AnalyticaW')
+
+
+@section('content')
+<h1>Operaciones</h1>
+<div class="container">
+    <div class="row">
+      <div class="col-md-6 col-lg-4" >
+        <div class="card" style="width: 18rem;" id="containerOne">
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4" >
+        <div class="card" style="width: 18rem;" id="containerTwo">
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 d-flex justify-content-center align-items-center">
+    <div>
+        <div class="card bg-primary" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Contactos registrados: <?= $dataContactos+1 ?></h5>
+            </div>
+        </div>
+        <div class="card bg-success" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Total bolsa de empleos: <?= $dataBE ?></h5>
+            </div>
+        </div>
+        <div class="card bg-secondary" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Lideres: <?= $dataEL ?></h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+      <div class="col-md-6 col-lg-4" >
+        
+      </div>
+      <div class="col-md-6 col-lg-4" id="containerFive">
+        <!-- Gráfico 5 -->
+      </div>
+      <div class="col-md-6 col-lg-4" id="containerSix">
+        <!-- Gráfico 6 -->
+      </div>
+      <div class="col-md-6 col-lg-4" id="containerSeven">
+        <!-- Gráfico 7 -->
+      </div>
+      <div class="col-md-6 col-lg-4" id="containerEight">
+        <!-- Gráfico 8 -->
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <!-- Gráfico 9 -->
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <!-- Gráfico 10 -->
+      </div>
+    </div>
+
+
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
+
+    <script>
+      // Data retrieved from https://netmarketshare.com
+        Highcharts.chart('containerOne', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Empleos',
+                align: 'left'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: <?= $dataEmpleos ?>,
+            }]
+        });
+
+        
+    </script>
+
+
+    <script>
+            Highcharts.chart('containerTwo', {
+          chart: {
+              type: 'column'
+          },
+          title: {
+              text: 'Duracion cursos'
+          },
+          xAxis: {
+              type: 'Cursos',
+              labels: {
+                  rotation: -45,
+                  style: {
+                      fontSize: '13px',
+                      fontFamily: 'Verdana, sans-serif'
+                  }
+              }
+          },
+          yAxis: {
+              min: 0,
+              title: {
+                  text: 'Duracion (Horas)'
+              }
+          },
+          legend: {
+              enabled: false
+          },
+          tooltip: {
+              pointFormat: 'Duracion: <b>{point.y:.1f} horas</b>'
+          },
+          series: [{
+              name: 'Population',
+              colors: [
+                  '#9b20d9', '#9215ac', '#861ec9', '#7a17e6', '#7010f9', '#691af3',
+                  '#6225ed', '#5b30e7', '#533be1', '#4c46db', '#4551d5', '#3e5ccf',
+                  '#3667c9', '#2f72c3', '#277dbd', '#1f88b7', '#1693b1', '#0a9eaa',
+                  '#03c69b',  '#00f194'
+              ],
+              colorByPoint: true,
+              groupPadding: 0,
+              data: <?= $dataCursos ?>,
+              dataLabels: {
+                  enabled: true,
+                  rotation: -90,
+                  color: '#FFFFFF',
+                  align: 'right',
+                  format: '{point.y:.1f}', // one decimal
+                  y: 10, // 10 pixels down from the top
+                  style: {
+                      fontSize: '13px',
+                      fontFamily: 'Verdana, sans-serif'
+                  }
+              }
+          }]
+      });
+
+
+    </script> 
+
+  <script>
+          // Data retrieved from https://netmarketshare.com/
+        Highcharts.chart('containerFour', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: {
+            text: 'Equipos',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 60
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%'],
+                size: '110%'
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Equipos',
+            innerSize: '50%',
+            data: <?= $dataContactos ?>
+        }]
+    });
+
+  </script> 
+
+
+
+@stop 
