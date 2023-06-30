3b5e3d03e467926837884f341432701f7a74408d
ByronAbata <bayronrocked97@gmail.com>
2023-06-30 07:17:29 -0500

    home vision y mision

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -147,7 +147,7 @@
          <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
                 <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="{{asset('assets/assets/img_aw/splash.jpg')}}" alt="..." />
+                <img class="masthead-avatar mb-5" src="{{asset('assets/assets/img_aw/chica.jpg')}}" alt="..." />
                 <!-- Masthead Heading-->
                 <h1 class="masthead-heading text-uppercase mb-0">Visiòn</h1>
                 <!-- Icon Divider-->
@@ -168,7 +168,7 @@
          <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
                 <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="{{asset('assets/assets/img_aw/splash.jpg')}}" alt="..." />
+                <img class="masthead-avatar mb-5" src="{{asset('assets/assets/img_aw/mision_img.jpg')}}" alt="..." />
                 <!-- Masthead Heading-->
                 <h1 class="masthead-heading text-uppercase mb-0">Misiòn</h1>
                 <!-- Icon Divider-->
@@ -186,43 +186,33 @@
                 </p>
             </div>
         </header>
-        <!-- Visiòn Section-->
-        <section class="page-section portfolio" id="portfolio">
-            <div class="container">
-                <!-- Portfolio Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">VISIÓN</h2>
+        <!-- Nuestra Líder Section-->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <!-- Masthead Avatar Image-->
+                
+                <!-- Masthead Heading-->
+                <h1 class="masthead-heading text-uppercase mb-0">Nuestra Líder</h1><br>
+                <img class="masthead-avatar mb-5" src="{{asset('assets/assets/img_aw/sara yanez.png')}}" alt="..." />
                 <!-- Icon Divider-->
-                <div class="divider-custom">
+                <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
                     <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                     <div class="divider-custom-line"></div>
                 </div>
-                <!-- Portfolio Grid Items-->
-                <div class="row justify-content-center">
-                    
-                    <!-- Portfolio Item 5-->
-                    <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
-                        <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <!-- <img class="img-fluid" src="{{asset('assets/assets/img_aw/splash.jpg')}}g" alt="..." /> -->
-                            <img class="masthead-avatar mb-5" src="{{asset('assets/assets/img_aw/logo.png')}}" alt="..." />
-                        </div> 
-                        <p><br>Ser una compañía ecuatoriana referente en el ámbito de la investigación financiera, económica, social y política, 
-                        aportando al bienestar y desarrollo humano, cumpliendo estándares de calidad, buenas prácticas y aplicando metodologías
-                        innovadoras en la investigación y análisis de información.</p>
-                    </div>
-                </div> 
-               
-                
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">
+                "Las mujeres debemos prepararnos constantemente en diferentes
+                 áreas del conocimiento, para aprovechar las oportunidades y enfrentar los desafíos"
+                </p>
             </div>
-        </section>
-        <!-- About Section-->
+        </header>
+        
+        <!-- Formato a 4 parafos Section-->
         <section class="page-section bg-primary text-white mb-0" id="about">
             <div class="container">
-                <!-- About Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                <!-- AnalytikaWomen Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">Analytika Women</h2>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -231,18 +221,25 @@
                 </div>
                 <!-- About Section Content-->
                 <div class="row">
-                    <div class="col-lg-4 ms-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-                    <div class="col-lg-4 me-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+
+                    <h1>Visión</h1>
+                    <div class="col-lg-4 ms-auto"><p class="lead">Ser una compañía ecuatoriana referente en el ámbito de la investigación financiera, económica, social y política, aportando al bienestar y desarrollo humano, cumpliendo estándares de calidad, 
+                        buenas prácticas y aplicando metodologías innovadoras en la investigación y análisis de información.</p></div>
+                    <h1>Misión</h1>
+                    <div class="col-lg-4 me-auto"><p class="lead">Transformar data en información mediante conceptos innovadores y ágiles, para que las 
+                        organizaciones e instituciones públicas y privadas, cuenten con las mejores soluciones en la toma de decisiones a nivel técnico,
+                         económico, político y social; considerando enfoques de tipo intergeneracional, intersectorial, intercultural, ambiental, de 
+                         género y movilidad.</p></div><br>
+                    <h1>CULTURA ORGANIZACIONAL</h1>
+                    <div class="col-lg-4 me-auto"><p class="lead">Nuestra empresa promueve la cultura de la integridad personal, para proponer las mejores alternativas frente a las necesidades o requerimientos solicitados; para lo cual nos apoyamos de un equipo
+                         multidisciplinario de profesionales con equidad de género.</p></div>
+                    
                 </div>
                 <!-- About Section Button-->
-                <div class="text-center mt-4">
-                    <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
-                        <i class="fas fa-download me-2"></i>
-                        Free Download!
-                    </a>
-                </div>
+                
             </div>
         </section>
+
         <!-- Contact Section-->
         <section class="page-section" id="contact">
             <div class="container">
